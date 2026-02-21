fae6fe04bd3ab13e10d8c3619c049c1438e7f82b
sauter-roland <sauter.roland@googlemail.com>
2026-02-21 11:02:03 +0100

    Content and format (#2)
    * adding navbar and more placeholder content
    
    * bugfixing, more content, remove default assets
    
    * smaller updates for better look and content

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         Hi! I'm Roland Sauter.
       </h1>
-      <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400 mt-4">
+      <p className="max-w-lg text-lg leading-8 text-zinc-600 dark:text-zinc-400 mt-4">
         I'm a software engineer, (data) scientist, and generally a nerd based in Oslo, Norway.
         This is my first attempt at a personal website, so expect it to evolve over time.
 
--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -5,7 +5,10 @@ export default function Projects() {
             <div className="space-y-8">
                 <div className="border-l-4 border-purple-500 pl-4">
                     <h2 className="text-2xl font-semibold">Coming soon...</h2>
-                    <p className="text-gray-600 dark:text-gray-400 mb-2">Description of project one.</p>
+                    <p className="text-gray-600 dark:text-gray-400 mb-2">
+                        The VPS this website is hosted on serves as the staging ground for some projects.
+                        Currently these are still under development.
+                    </p>
                 </div>
             </div>
         </div>
